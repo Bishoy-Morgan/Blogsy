d59fc89090e015584a9209a0e1cd9cdd5126bc90
Bishoy Morgan <bishoy.morgan95@gmail.com>
2025-06-30 11:34:11 +0300

    navbar-links

--- website/__init__.py
+++ website/__init__.py
@@ -2,12 +2,11 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'morgan__flask__notes'
+    app.config['SECRET_KEY'] = 'blogsy__multiuser'
 
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
-
     return app
