981c26e6a73b4044ce19213d579378f5ec95695e
Bishoy Morgan <bishoy.morgan95@gmail.com>
2025-06-30 23:59:12 +0300

    styles

--- website/views.py
+++ website/views.py
@@ -4,6 +4,10 @@ from flask_login import login_required, current_user
 views = Blueprint('views', __name__)
 
 @views.route('/')
+def welcome():
+    return render_template('welcome.html', user=current_user)
+
+@views.route('/home')
 @login_required
 def home():
     return render_template('home.html', user=current_user)
