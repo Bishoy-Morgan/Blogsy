78c80e8c67e74f75b351462255954bb43a4590f5
Bishoy Morgan <bishoy.morgan95@gmail.com>
2025-07-01 19:16:25 +0300

    home and blogs

--- website/auth.py
+++ website/auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
-from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, login_required, logout_user, current_user
 
 
@@ -47,13 +47,17 @@ def sign_up():
         if len(email) < 4:
             flash('Email must be greater than 3 characters.', category='error')
         elif len(first_name) < 2:
-            flash('First name must be greater than 1 character.', category='error')
+            flash('Name must be greater than 2 characters.', category='error')
         elif password1 != password2:
             flash('Passwords don\'t match.', category='error')
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
-            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='pbkdf2:sha256'))
+            new_user = User(
+                email = email, 
+                first_name = first_name,
+                password = generate_password_hash(password1, method='pbkdf2:sha256')
+            )
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
--- website/views.py
+++ website/views.py
@@ -1,5 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
+from . import db
+from .models import Blog
+from flask_login import current_user
 
 views = Blueprint('views', __name__)
 
@@ -10,5 +13,29 @@ def welcome():
 @views.route('/home')
 @login_required
 def home():
-    return render_template('home.html', user=current_user)
+    blogs = Blog.query.order_by(Blog.date_created.desc()).all()
+    return render_template('home.html', user=current_user, blogs=blogs)
+
+@views.route('/write', methods=['GET', 'POST'])
+@login_required
+def write():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        content = request.form.get('content')
+
+        if len(title) < 5:
+            flash('Title must be greater than 5 characters.', category='error')
+        elif len(content) < 100:
+            flash('Content must be at least 100 characters.', category='error')
+        else:
+            new_blog = Blog(
+                title = title,
+                content = content,
+                author_id=current_user.id
+            )
+            db.session.add(new_blog)
+            db.session.commit()
+            flash('Post created successfully!', category='success')
+            return redirect(url_for('views.home'))
+    return render_template('write.html', user=current_user)
 
